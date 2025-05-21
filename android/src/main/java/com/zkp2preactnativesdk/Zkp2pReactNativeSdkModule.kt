6f65aa305398c7377f333c33cb23ed71e2420d82
Richard <richardliang2015@u.northwestern.edu>
2025-05-21 12:58:10 -0700

    fix: android build

--- android/src/main/java/com/zkp2preactnativesdk/Zkp2pReactNativeSdkModule.kt
+++ android/src/main/java/com/zkp2preactnativesdk/Zkp2pReactNativeSdkModule.kt
@@ -1,6 +1,10 @@
 package com.zkp2preactnativesdk
 
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.WritableNativeArray
+import com.facebook.react.bridge.WritableNativeMap
 import com.facebook.react.module.annotations.ReactModule
 
 @ReactModule(name = Zkp2pReactNativeSdkModule.NAME)
@@ -11,10 +15,46 @@ class Zkp2pReactNativeSdkModule(reactContext: ReactApplicationContext) :
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
-  override fun multiply(a: Double, b: Double): Double {
-    return a * b
+  override fun startAuthentication(provider: ReadableMap, promise: Promise) {
+    println("Native: startAuthentication called with provider: " + provider.toString())
+    promise.resolve(null)
+  }
+
+  override fun handleSuccess(provider: ReadableMap, promise: Promise) {
+    println("Native: handleSuccess called with provider: " + provider.toString())
+    promise.resolve(null)
+  }
+
+  override fun handleError(errorMessage: String, promise: Promise) {
+    println("Native: handleError called with errorMessage: " + errorMessage)
+    promise.resolve(null)
+  }
+
+  override fun generateProof(
+    provider: ReadableMap,
+    transaction: ReadableMap,
+    interceptedPayload: ReadableMap,
+    intentHash: String,
+    promise: Promise
+  ) {
+    println("Native: generateProof called with intentHash: " + intentHash)
+    val result = WritableNativeMap()
+    promise.resolve(result)
+  }
+
+  override fun handleIntercept(event: ReadableMap, promise: Promise) {
+    println("Native: handleIntercept called with event: " + event.toString())
+    promise.resolve(null)
+  }
+
+  override fun extractTransactionsData(
+    provider: ReadableMap,
+    jsonResponseBody: String,
+    promise: Promise
+  ) {
+    println("Native: extractTransactionsData called with jsonResponseBody: " + jsonResponseBody)
+    val result = WritableNativeArray()
+    promise.resolve(result)
   }
 
   companion object {
